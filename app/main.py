7dc3c32ca30ad22f5c9f889818c0017fe29e7247
SensibleN00B <sensible.noob@gmail.com>
2025-06-23 15:19:37 +0300

    Fixed type annotation of brand parametr

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ class Car:
     def __init__(self,
                  comfort_class: int,
                  clean_mark: int,
-                 brand: int
+                 brand: str
                  ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
